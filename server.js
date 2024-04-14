c9d0f06b252dcfb748e474f3519e1d203d3b7b06
강희원 <kang1234536@github.com>
2024-04-14 16:20:32 +0900

    test2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // .env
-require("dotenv").config();
+require("dotenv").config(); 
 
 const jwt       = require('jsonwebtoken');
 const secretKey = process.env.SECRETKEY;
