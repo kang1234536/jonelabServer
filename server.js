31e680804469751c560bf8be7e2d457296309bed
강희원 <kha0202kha@naver.com>
2024-04-14 10:50:47 +0900

    24.04.14 작업

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+app.use(passport.initialize());
+app.use(session({
+  secret: 'jonelabSecret',
+  resave : false,
+  saveUninitialized : false
+}))
+app.use(passport.session()); 
 
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
@@ -15,6 +27,10 @@ new MongoClient(url).connect().then((client)=>{
   console.log(err)
 })
 
+// app.listen(6060, () => {
+//     console.log('서버 실행중')
+// })
+
 app.listen(process.env.PORT || 6060, () => {
     console.log('서버 실행중')
 })
@@ -23,10 +39,45 @@ app.get('/', (req, res) => {
   res.send('반갑다')
 }) 
 
-app.post('/', async(req, res) => {
+app.post('/join', async(req, res) => {
     console.log(req.body);
     console.log('클라이언트 접속완료')
     // let result = await db.collection('COM_USER_INFO').find().toArray()
     let result = await db.collection('COM_USER_INFO').findOne({user_id : req.body.user_id, user_pw :req.body.user_pw})
     res.send(result)
 }) 
+
+app.post('/login', async(req, res) => {
+    console.log(req.body);
+    console.log('클라이언트 접속완료')
+    // let result = await db.collection('COM_USER_INFO').find().toArray()
+    let result = await db.collection('COM_USER_INFO').findOne({user_id : req.body.user_id, user_pw :req.body.user_pw})
+    res.send(result)
+});
+
+passport.use(new LocalStrategy(async (user_id, user_pw, cb) => {
+    let result = await db.collection('COM_USER_INFO').findOne({ user_id : user_id})
+    if (!result) {
+      return cb(null, false, { message: '아이디 DB에 없음' })
+    }
+    if (result.password == user_pw) {
+      return cb(null, result)
+    } else {
+      return cb(null, false, { message: '비번불일치' });
+    }
+  }))
+
+
+app.post('/login2', async(req, res, next) => {
+    console.log('??')
+    passport.authenticate('local', (error, user, info) => {
+        if (error) return res.status(500).json(error)
+        if (!user) return res.status(401).json(info.message)
+        req.logIn(user, (err) => {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+    })(req, res, next)
+
+}); 
+
